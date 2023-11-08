d72b31150e36553c083e545eab43ec89984c0ef2
KaueSanchesG <kauesanches123@gmail.com>
2023-11-08 10:45:30 -0300

    bug

---
+++ src/main/java/com/security/spring/config/Security.java
@@ -0,0 +1,42 @@
+package com.security.spring.config;
+
+import com.security.spring.user.UserAuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class Security {
+    @Autowired
+    private UserAuthService service;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(cors-> cors.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/livre").permitAll()
+                        .anyRequest().authenticated());
+        http.httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        return service;
+    }
+}
